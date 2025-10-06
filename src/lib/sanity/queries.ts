075eca03618df5feafeea629e09dc12bb694e5f7
JiMarchel <jmarchel100@gmail.com>
2025-10-06 08:22:04 +0700

    feat: add Sanity queries for categories and posts

---
+++ src/lib/sanity/queries.ts
@@ -0,0 +1,70 @@
+export const GET_ALL_CATEGORIES = `
+*[_type == "category"] | order(title asc) {
+  _id,
+  title,
+  "slug": slug.current,
+  description,
+}
+`;
+
+export const POSTS_BY_ALL_CATEGORIES = `
+*[
+  _type=="post" &&
+  count(categories[@->slug.current in $slugs]) == count($slugs)
+]
+| order(publishedAt desc){
+  _id, title, "slug": slug.current, publishedAt,
+  "excerpt": select(
+    defined(excerpt) && excerpt != "" => excerpt,
+    defined(body) => pt::text(body)[0...180],
+    ""
+  ),
+  mainImage{ "url": asset->url, "dims": asset->metadata.dimensions },
+  categories[]->{ title, "slug": slug.current }
+}
+`;
+
+export const POSTS_BY_ANY_CATEGORY = `
+*[
+  _type=="post" &&
+  count(categories[@->slug.current in $slugs]) > 0
+]
+| order(publishedAt desc){
+  _id, title, "slug": slug.current,
+  mainImage{ "url": asset->url, "dims": asset->metadata.dimensions },
+  publishedAt,
+  "excerpt": select(defined(excerpt) && excerpt != "" => excerpt, defined(body) => pt::text(body)[0...180], ""),
+  categories[]->{ title, "slug": slug.current }
+}
+`;
+
+export const POSTS_QUERY = `*[_type == "post" && defined(slug.current)]
+| order(publishedAt desc)[0...12]{
+  _id,
+  title,
+  "slug": slug.current,
+  mainImage{
+    ...,
+    "url": asset->url,
+    "dims": asset->metadata.dimensions
+  },
+  publishedAt,
+  categories[]->{ title, "slug": slug.current }
+}`;
+
+export const POST_QUERY = `
+*[_type == "post" && slug.current == $slug][0]{
+  _id,
+  title,
+  "slug": slug.current,
+  body,                             
+  mainImage{
+    ...,
+    "url": asset->url,
+    "dims": asset->metadata.dimensions
+  },
+  "excerpt": select(defined(excerpt) && excerpt != "" => excerpt, defined(body) => pt::text(body)[0...180], ""),
+  publishedAt,
+  categories[]->{ title, "slug": slug.current }
+}
+`;
