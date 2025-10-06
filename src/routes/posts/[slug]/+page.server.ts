128a8b5d53caa9a1085aae8677ac46178c718be3
JiMarchel <jmarchel100@gmail.com>
2025-10-06 10:08:54 +0700

    refactor: remove console log from load function in +page.server.ts

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -3,7 +3,6 @@ import { POST_QUERY } from "$lib/sanity/queries";
 
 export async function load({ params }) {
   const post = await client.fetch(POST_QUERY, { slug: params.slug });
-  console.log(post);
 
   if (!post) {
     return { status: 404 };
