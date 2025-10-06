8e0ffc811ba63f7ff0c36ba85f5cafd63195b52f
JiMarchel <jmarchel100@gmail.com>
2025-10-06 08:20:23 +0700

    feat: add navbar component with social media links and navigation

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import {
+    Github,
+    House,
+    Instagram,
+    Linkedin,
+    Mail,
+    PencilLine,
+  } from "lucide-svelte";
+</script>
+
+<nav
+  class="border-b border-gray py-2 px-1 flex pt-5 items-center justify-between gap-2 shadow-2xl w-2xl"
+>
+  <div class="flex items-center gap-4">
+    <a href="/" class="hover:scale-125 transition-all duration-200">
+      <House />
+    </a>
+    <a href="/posts" class="hover:scale-125 transition-all duration-200">
+      <PencilLine />
+    </a>
+  </div>
+  <div class="flex items-center gap-4 ">
+    <a href="https://github.com/JiMarchel" target="_blank" class="hover:scale-125 transition-all duration-200"><Github /></a>
+    <a href="https://www.linkedin.com/in/jimarchel/" target="_blank" class="hover:scale-125 transition-all duration-200"><Linkedin /></a>
+    <a href="https://www.instagram.com/0xmarchel/" target="_blank" class="hover:scale-125 transition-all duration-200"><Instagram /></a>
+    <a href="mailto:dev@jimarchel.my.id" class="hover:scale-125 transition-all duration-200"><Mail /></a>
+  </div>
+</nav>
