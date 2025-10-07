e46e47391c20a1a71f6166057fd0a9338f58b26e
JiMarchel <jmarchel100@gmail.com>
2025-10-07 13:22:41 +0700

    refactor: simplify custom list component by removing unused props and conditional rendering

--- src/lib/components/custom-list.svelte
+++ src/lib/components/custom-list.svelte
@@ -1,28 +1,7 @@
 <script lang="ts">
-  import type { BlockComponentProps } from "@portabletext/svelte";
-  import type { Snippet } from "svelte";
-
-  interface Props {
-    portableText: BlockComponentProps;
-    children: Snippet;
-  }
-
-  let { portableText, children }: Props = $props();
-
-  let { indexInParent, global, value } = $derived(portableText);
-  let { ptBlocks } = $derived(global);
-  let { style } = $derived(value);
+  let { children } = $props();
 </script>
 
-<!-- If preceded by heading, have a higher margin top -->
-<div class="relative flex items-center gap-1">
-  {#if style === "bullet"}
-    <ul class="list-disc pl-5">
-      <li>{@render children()}</li>
-    </ul>
-  {:else if style === "number"}
-    <ol class="list-decimal pl-5">
-      <li>{@render children()}</li>
-    </ol>
-  {/if}
-</div>
+<li class="list-disc ml-7">
+  {@render children()}
+</li>
